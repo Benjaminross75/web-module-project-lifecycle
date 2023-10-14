5bbe8614506f0e7b784392020c9e995e7f49fff8
Ben Ross <mrbenross@gmail.com>
2023-10-14 17:03:16 -0500

    Form.js and Todo.js updated

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -2,6 +2,24 @@ import React from 'react'
 
 export default class Form extends React.Component {
   render() {
-    return null
+    return (
+      <>
+      <form id="todoForm" onSubmit={this.props.onTodoFormSubmit}>
+          <input
+             onChange={this.props.onTodoNameInputChange}
+             value={this.props.todoNameInput}
+             type="text"
+             placeholder='Type todo'>
+          </input>
+          <input type='submit'></input>
+
+        </form>
+        <button
+           onClick={this.props.toggleDisplayCompleteds}
+           >
+            {this.props.displayCompleteds ? 'Hide' : 'Show'}Completed
+            </button>
+        </>
+    )
   }
 }
