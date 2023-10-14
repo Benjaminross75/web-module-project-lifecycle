d8a6e3a764496062c1bd755b5f8d790c8ef5b133
Ben Ross <mrbenross@gmail.com>
2023-10-14 16:22:48 -0500

    toggle display completed method added to App.js

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -7,6 +7,7 @@ export default class App extends React.Component {
     todos: [],
     error: '',
     todoNameInput: '',
+    displayCompleteds: true,
 
   }
 
@@ -54,6 +55,10 @@ export default class App extends React.Component {
     })
     .catch(this.errorResponse)
   }
+
+  toggleDisplayCompleteds = () =>{
+    this.setState({...this.state, displayCompleteds: !this.state.displayCompleteds})
+  }
   componentDidMount(){
     this.fetchAllTodos()
   }
@@ -64,16 +69,21 @@ export default class App extends React.Component {
         <div id="todos">
           <h2>Todos:</h2>
           {
-            this.state.todos.map(td =>{
-              return <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? ' ✅' : ''}</div>
-            })
+            this.state.todos.reduce((acc, td)=>{
+                if(this.state.displayCompleteds || !td.completed) return acc.concat(
+                  <div onClick={this.toggleCompleted(td.id)} key={td.id}>{td.name} {td.completed ? ' ✅' : ''}</div>
+                )
+                return acc
+            },[])
+           
           }
         </div>
         <form id="todoForm" onSubmit={this.onTodoFormSubmit}>
           <input onChange={this.onTodoNameInputChange} value={this.state.todoNameInput} type="text" placeholder='Type todo'></input>
           <input type='submit'></input>
-          <button>Clear Completed</button>
+
         </form>
+        <button onClick={this.toggleDisplayCompleteds}>{this.state.displayCompleteds ? 'Hide' : 'Show'}Completed</button>
       </div>
     )
   }
